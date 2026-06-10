5860b20b026860f31fba0ac40d53c3acebf0c9e4
IbraUA <sergii.ezhov@gmail.com>
2026-06-10 11:48:13 +0300

    feat: random command with buttons

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
+import app
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler
 
 from credentials import TELEGRAM_TOKEN, OPENAI_TOKEN
 from gpt import ChatGptService
 from util import (load_message, send_text, send_image, show_main_menu,
-                  default_callback_handler)
+                  default_callback_handler, load_prompt, send_text_buttons)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -21,20 +22,37 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         # 'command': 'button text'
 
     })
+async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await send_image(update, context, 'random')
+    await send_text(update, context, text)
 
+    prompt = load_prompt('random')
+    answer = await chat_gpt.send_question(prompt, '')
 
-
+    await send_text_buttons(update, context, answer, {
+        'random': 'Хочу ще факт 🧠',
+        'start': 'Закінчити'
+    })
 
 chat_gpt = ChatGptService(OPENAI_TOKEN)
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 app.add_handler(CommandHandler('start', start))
+app.add_handler(CommandHandler('random', random))
+text = load_message('random')
+async def random_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.callback_query.answer()
+    await random(update, context)
+async def start_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.callback_query.answer()
+    await start(update, context)
+
+app.add_handler(CallbackQueryHandler(random_callback, pattern='^random$'))
+app.add_handler(CallbackQueryHandler(start_callback, pattern='^start$'))
+app.add_handler(CallbackQueryHandler(default_callback_handler))
+
 app.add_handler(CallbackQueryHandler(default_callback_handler))
 app.run_polling()
 
-# Зареєструвати обробник команди можна так:
-# app.add_handler(CommandHandler('command', handler_func))
 
-# Зареєструвати обробник колбеку можна так:
-# app.add_handler(CallbackQueryHandler(app_button, pattern='^app_.*'))
-app.add_handler(CallbackQueryHandler(default_callback_handler))
 app.run_polling()
+
